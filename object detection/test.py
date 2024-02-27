15e1b60d7e0feef4ec57a6c5b7c77828db23d91d
navajyoth puthalath <106015664+navajyoth-puthalath13@users.noreply.github.com>
2024-02-27 17:20:55 +0530

    update in object dectection

--- object detection/test.py
+++ object detection/test.py
@@ -7,7 +7,7 @@ KNN_subtractor = cv2.createBackgroundSubtractorKNN(detectShadows=True)
 
 bg_subtractor = KNN_subtractor
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture("desktop")
 
 while True:
     ret, frame = camera.read()
